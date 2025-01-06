cd50eb166362bc0d5104c09022f11e0a3b7a7d4f
mdrakibmia99 <rkrakibhasan680@gmail.com>
2025-01-06 23:17:06 +0600

    update content and upload quizData

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import { Button } from "@/components/ui/button"
+import { Question } from "./Home/Question";
 
 export default function Home() {
-
   return (
-    <div>
-      <Button>Click me</Button>
-    </div>
-  )
+    <>
+      <Question />
+    </>
+  );
 }
--- src/Home/Question.tsx
+++ src/Home/Question.tsx
@@ -1,6 +1,28 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
-export default function Question() {
+
+export function Question() {
   return (
-    <div>Question</div>
-  )
+    <Card className="w-[350px]">
+      <CardHeader>
+        <CardTitle>Create project</CardTitle>
+        <CardDescription>Deploy your new project in one-click.</CardDescription>
+      </CardHeader>
+      <CardContent>
+
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <Button variant="outline">Cancel</Button>
+        <Button>Deploy</Button>
+      </CardFooter>
+    </Card>
+  );
 }
