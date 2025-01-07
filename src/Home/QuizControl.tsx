c4f36b3f91e5347c5a96a81a0fa2eaf63a19577a
mdrakibmia99 <rkrakibhasan680@gmail.com>
2025-01-07 15:30:07 +0600

    update quizController

--- src/Home/QuizControl.tsx
+++ src/Home/QuizControl.tsx
@@ -1,19 +1,49 @@
 import { Button } from "@/components/ui/button";
-import { nextQuestion, previousQuestion } from "@/redux/features/quiz/quizSlice";
-import { useAppDispatch } from "@/redux/hooks";
+import {
+    completeQuestion,
+  nextQuestion,
+  previousQuestion,
+} from "@/redux/features/quiz/quizSlice";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 
 const QuizControl = () => {
   const dispatch = useAppDispatch();
+  const { currentQuestionIndex, question, userAnswer, quizComplete } =
+    useAppSelector((state) => state.quiz);
+  const isAnswerQuiz = userAnswer[currentQuestionIndex] !== null;
   const handleNextQuestion = () => {
     dispatch(nextQuestion());
   };
   const handlePreviousQuestion = () => {
     dispatch(previousQuestion());
   };
+  const handleCompletedQuestion=() => {
+    dispatch(completeQuestion());
+  }
+  const isCompleteQuiz =
+    isAnswerQuiz || currentQuestionIndex !== question.length - 1;
+
   return (
     <div className="flex justify-between p-6">
-      <Button onClick={handlePreviousQuestion}> Previous</Button>
-      <Button onClick={handleNextQuestion}> Next</Button>
+      <Button
+        disabled={currentQuestionIndex === 0 || quizComplete}
+        onClick={handlePreviousQuestion}
+      >
+        {" "}
+        Previous
+      </Button>
+      {currentQuestionIndex < question.length - 1 && (
+        <Button disabled={!isAnswerQuiz} onClick={handleNextQuestion}>
+          {" "}
+          Next
+        </Button>
+      )}
+
+      {currentQuestionIndex === question.length - 1 && !quizComplete && (
+        <Button
+        onClick={handleCompletedQuestion}
+        disabled={!isCompleteQuiz}> Quiz Complete</Button>
+      )}
     </div>
   );
 };
