908fbc6167769b0e63402aeb2f225b0c3423c00b
mdrakibmia99 <rkrakibhasan680@gmail.com>
2025-01-07 15:30:46 +0600

    update quizSummery

--- src/Home/QuizSummery.tsx
+++ src/Home/QuizSummery.tsx
@@ -1,6 +1,18 @@
+import { Card, CardHeader } from "@/components/ui/card";
+import { useAppSelector } from "@/redux/hooks";
 
 export default function QuizSummery() {
+  const { currentQuestionIndex, question, userAnswer, quizComplete } =
+    useAppSelector((state) => state.quiz);
+  const correctAnserCount= question.reduce((count,qn,index)=>{
+ return qn.correctAnswer === userAnswer[index] ? count+1:count
+  },0)
+  console.log(correctAnserCount)
   return (
-    <div>QuizSummery</div>
-  )
+    <Card className="max-w-lg mx-auto p-6 rounded-xl shadow-xl">
+      <CardHeader>
+        <h2 className="text-2xl font-bold">Quiz Summary</h2>
+      </CardHeader>
+    </Card>
+  );
 }
