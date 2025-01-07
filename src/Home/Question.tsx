4bba2e77375c5350376e7f81c8952bf46f616df6
mdrakibmia99 <rkrakibhasan680@gmail.com>
2025-01-07 13:46:16 +0600

    update question.tsx file using dispatch

--- src/Home/Question.tsx
+++ src/Home/Question.tsx
@@ -7,17 +7,31 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-
+import { setAnswer } from "@/redux/features/quiz/quizSlice";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 
 export function Question() {
+  const dispatch=useAppDispatch()
+  const { currentQuestionIndex, question, quizComplete, userAnswer } =
+    useAppSelector((state) => state.quiz);
+    const currentQuestion = question[currentQuestionIndex];
+    const handleQuestionAnswerChange=(answer:string)=>{
+      dispatch(setAnswer({questionIndex:currentQuestionIndex,answer}))
+    }
   return (
     <Card className="w-[350px]">
       <CardHeader>
-        <CardTitle>Create project</CardTitle>
-        <CardDescription>Deploy your new project in one-click.</CardDescription>
+        <CardTitle>{currentQuestion.options}</CardTitle>
+        <CardDescription>
+          Question:{currentQuestionIndex + 1} of {question.length}{" "}
+        </CardDescription>
       </CardHeader>
       <CardContent>
-
+        {currentQuestion.options.map((option, index) => (
+          <Button onClick={()=>handleQuestionAnswerChange(option)} key={index} size={"lg"} className="w-full mt-5">
+            {option}
+          </Button>
+        ))}
       </CardContent>
       <CardFooter className="flex justify-between">
         <Button variant="outline">Cancel</Button>
