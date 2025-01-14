d6fa26adc4747da289d04d9b4bb23bdcc7d49de0
mdrakibmia99 <rkrakibhasan680@gmail.com>
2025-01-14 22:25:05 +0600

    update app page

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
-import { Question } from "./Home/Question";
-import QuizSummery from "./Home/QuizSummery";
-import { Result } from "./Home/Result";
-import { useAppSelector } from "./redux/hooks";
+import MainLayout from "./components/layout/MainLayout";
+import ProtectedRoute from "./components/layout/ProtectedRoute";
+
+
 
 export default function App() {
-  const { quizComplete, moreResultInfo } = useAppSelector(
-    (state) => state.quiz
-  );
+  // const { quizComplete, moreResultInfo } = useAppSelector(
+  //   (state) => state.quiz
+  // );
   return (
-    <div>
-      <h1 className="text-center text-9xl my-12">Quiz App</h1>
-      {
-        moreResultInfo?<Result /> :(quizComplete ? <QuizSummery /> : <Question />)
-      }
-      
-
-     
-    </div>
+    <ProtectedRoute>
+      <MainLayout/>
+      {/* <div>
+        <Header />
+        <h1 className="text-center text-9xl my-12">Quiz App</h1>
+        {moreResultInfo ? (
+          <Result />
+        ) : quizComplete ? (
+          <QuizSummery />
+        ) : (
+          <Question />
+        )}
+      </div> */}
+    </ProtectedRoute>
   );
 }
