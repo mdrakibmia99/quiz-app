54e15a0bf320fdad3625fe963014c310d52c85f1
mdrakibmia99 <rkrakibhasan680@gmail.com>
2025-01-14 22:26:09 +0600

    modify login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { useForm } from "react-hook-form";
+import { FieldValues, useForm } from "react-hook-form";
 
 // import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
@@ -12,21 +12,46 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { PasswordInput } from "@/components/ui/password-input";
+import { useLoginMutation } from "@/redux/features/auth/authApi";
+import { toast } from "sonner";
+import { verifyToken } from "@/utils/verifyToken";
+import { useAppDispatch } from "@/redux/hooks";
+import { setUser } from "@/redux/features/auth/authSlice";
 
 export default function Login() {
+    const [login] = useLoginMutation();
+    const dispatch = useAppDispatch();
+    const navigate = useNavigate();
   const form = useForm({
     defaultValues: {
-      email: "",
-      password: "",
+      email: "admin@gmail.com",
+      password: "@Admin123",
     },
   });
 
-  function onSubmit(values: any) {
+  async function onSubmit(values: FieldValues) {
     console.log(values);
-  }
+    const toastId = toast.loading('Logging in');
+    try{
+        const userInfo={
+            email:values.email,
+            password:values.password
+        }
+        const res=await login(userInfo).unwrap();
+        console.log(res,"res")
+        const user = verifyToken(res.data.token)
+        dispatch(setUser({user:user,token:res?.data.token}))
+        toast.success('Logged in', { id: toastId, duration: 2000 });
+        navigate(`/`);
 
+        console.log(res,"rest")
+    }catch{
+        toast.error('Something went wrong', { id: toastId, duration: 2000 });
+    }
+  }
+console.log(login,"login data")
   return (
     <div className="h-svh w-full grid place-items-center p-10">
       <Form {...form}>
