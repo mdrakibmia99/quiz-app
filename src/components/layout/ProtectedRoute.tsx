bbf3c6d15d5f566da70ad31514ade076d45b0255
mdrakibmia99 <rkrakibhasan680@gmail.com>
2025-01-14 22:25:14 +0600

    create protection route

--- src/components/layout/ProtectedRoute.tsx
+++ src/components/layout/ProtectedRoute.tsx
@@ -1,10 +1,14 @@
+import { selectCurrentToken } from "@/redux/features/auth/authSlice";
+import { useAppSelector } from "@/redux/hooks";
+import { ReactNode } from "react";
+import { Navigate } from "react-router-dom";
 
-const ProtectedRoute = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const ProtectedRoute = ({ children }: { children: ReactNode }) => {
+  const token = useAppSelector(selectCurrentToken);
+  if (!token) {
+    return <Navigate to={"/login"} replace={true} />;
+  }
+  return children;
 };
 
 export default ProtectedRoute;
