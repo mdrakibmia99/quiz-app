0e63a5b4693de9d6ed9889539275c35043c033fc
mdrakibmia99 <rkrakibhasan680@gmail.com>
2025-01-14 22:26:28 +0600

    update store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,19 +1,42 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { quizSlice } from "./features/quiz/quizSlice";
-import { quizApi } from "./api/quiz/quizApi";
+import authSlice from "./features/auth/authSlice";
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { baseApi } from "./api/baseApi";
+
+const persistConfig = {
+  key: "auth",
+  storage,
+};
+
+const persistedAuthReducer = persistReducer(persistConfig, authSlice);
 
 export const store = configureStore({
   reducer: {
     quiz: quizSlice.reducer,
-    [quizApi.reducerPath]: quizApi.reducer,
+    [baseApi.reducerPath]: baseApi.reducer,
+    auth: persistedAuthReducer,
   },
-  middleware: (getDefaultMiddleware) => {
-    return getDefaultMiddleware().concat(quizApi.middleware);
-  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(baseApi.middleware),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
-
+export const persistor = persistStore(store);
