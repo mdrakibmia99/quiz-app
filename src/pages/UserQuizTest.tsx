e5d9c560800f6d92b128bcf94ccb8524dd4c2c3e
mdrakibmia99 <rkrakibhasan680@gmail.com>
2025-01-17 18:31:15 +0600

    update user quiz test

--- src/pages/UserQuizTest.tsx
+++ src/pages/UserQuizTest.tsx
@@ -4,40 +4,55 @@ import QuizSummery from "@/Home/QuizSummery";
 import { Result } from "@/Home/Result";
 import { useGetSingleQuizQuery } from "@/redux/features/quiz/quizApi";
 import { setQuiz } from "@/redux/features/quiz/quizSlice";
+import { useUserQuizResultQuery } from "@/redux/features/quizResult/quizResultApi";
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
 const UserQuizTest = () => {
   const { id } = useParams();
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
+  const [mainLoading,setMainLoading]=useState(false)
+
+
   const { quizComplete, moreResultInfo ,} = useAppSelector(
     (state) => state.quiz
   );
   const { data, isLoading } = useGetSingleQuizQuery(id);
+  const { data:quizResultData, isLoading:quizResultLoading } = useUserQuizResultQuery(id);
+
+  useEffect(()=>{
+    setMainLoading(true)
+    if(!isLoading && data){
+      console.log(data,"check quiz data")    
+      dispatch(setQuiz(data.data.questions))
+      console.log(data,"check quiz dataasdfasdfasdfsf")
+      setMainLoading(false)    
+    }
+    setMainLoading(false)
+  },[isLoading,data])
   console.log(id, "check user id");
   if (!isLoading && !data) {
     navigate("/");
   }
-  if(!isLoading && data){
-    console.log(data,"check quiz data")    
-    dispatch(setQuiz(data.data.questions))
-  }
-  if (isLoading) {
+  
+  if (isLoading || mainLoading || quizResultLoading) {
     return <div>Loading...</div>;
   }
-  console.log(data,"check user")
+console.log(quizResultData,"quizResultData")
   return (
     <div className="w-full h-full">
       <Header />
       <div className="grid place-items-center min-h-svh w-full">
         {moreResultInfo ? (
           <Result />
-        ) : quizComplete ? (
+        ) : quizResultData?.data?.quizComplete ? (
           <QuizSummery />
         ) : (
           <Question />
-        )}
+          )}
+
       </div>
     </div>
   );
