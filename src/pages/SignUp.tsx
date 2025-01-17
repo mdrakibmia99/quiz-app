0e48e5021cc63b7e21c5a0eb54363a3790e38b16
mdrakibmia99 <rkrakibhasan680@gmail.com>
2025-01-18 02:36:46 +0600

    signup page update button

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -121,7 +121,7 @@ export default function SignUp() {
             />
 
             <Button type="submit" className="w-full">
-              Submit
+              SIGN UP
             </Button>
           </form>
 
