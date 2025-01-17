be65ed25d8fa186e5cc19acb25373f2b9501d0bd
mdrakibmia99 <rkrakibhasan680@gmail.com>
2025-01-17 21:31:34 +0600

    udpate sing up page

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,7 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { useForm } from "react-hook-form";
-
-// import { cn } from "@/lib/utils";
+import { FieldValues, useForm } from "react-hook-form";
 import { Button } from "@/components/ui/button";
 import {
   Form,
@@ -14,70 +12,109 @@ import {
 import { Input } from "@/components/ui/input";
 import { Link, useNavigate } from "react-router-dom";
 import { PasswordInput } from "@/components/ui/password-input";
+import { useSignUpMutation } from "@/redux/features/auth/authApi";
+import { toast } from "sonner";
 
 export default function SignUp() {
-  const navigate=useNavigate()
+  const navigate = useNavigate();
+  const [signUp] = useSignUpMutation();
+  // Use `react-hook-form` with validation schema
   const form = useForm({
     defaultValues: {
       name: "",
       email: "",
       password: "",
     },
+    mode: "onBlur", // Validate fields when they lose focus
   });
 
-  function onSubmit(values: any) {
-    console.log(values);
+  async function onSubmit(values: FieldValues) {
+    try {
+      const result = await signUp(values);
+      console.log(result, "result");
+      if (result.error) {
+        toast.error("Email already exists");
+      } else {
+        toast.success("Signed up successfully", { duration: 2000 });
+        form.reset();
+        navigate('/login')
+      }
+    } catch (error) {
+      if ((error as any).statusCode == 409) {
+        toast.error("ALready signed up use this Email");
+      } else {
+        toast.error("Something went wrong");
+      }
+    }
   }
 
   return (
     <div className="h-svh w-full grid place-items-center">
       <Form {...form}>
-        <div className="mx-auto p-1 relative lg:p-10 border-b-slate-600 border-2 rounded-sm w-full lg:w-96 md:w-60">
-        <p onClick={()=>navigate('/')} className="border inline font-bold shadow-md hover:shadow-sm hover:cursor-pointer px-3 py-1 rounded-full absolute top-0 right-0 m-2">X</p>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 ">
+        <div className="mx-auto p-1 relative lg:p-10 md:p-10 border-b-slate-600 border-2 rounded-sm w-full lg:w-96 md:w-96">
+          <p
+            onClick={() => navigate("/")}
+            className="border inline font-bold shadow-md hover:shadow-sm hover:cursor-pointer px-3 py-1 rounded-full absolute top-0 right-0 m-2"
+          >
+            X
+          </p>
+          <h2 className="text-center mt-5 text-2xl font-semibold uppercase my-3">sign up</h2>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            {/* Name Field */}
             <FormField
               control={form.control}
               name="name"
+              rules={{ required: "Name is required" }}
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel>Email</FormLabel>
+                  <FormLabel>Name</FormLabel>
                   <FormControl>
                     <Input placeholder="Name" type="text" {...field} />
                   </FormControl>
-                  {/* <FormDescription>
-                  This is your public display name.
-                </FormDescription> */}
                   <FormMessage />
                 </FormItem>
               )}
             />
+
+            {/* Email Field */}
             <FormField
               control={form.control}
               name="email"
+              rules={{
+                required: "Email is required",
+                pattern: {
+                  value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                  message: "Enter a valid email",
+                },
+              }}
               render={({ field }) => (
                 <FormItem>
                   <FormLabel>Email</FormLabel>
                   <FormControl>
                     <Input placeholder="Email" type="email" {...field} />
                   </FormControl>
-                  {/* <FormDescription>
-                  This is your public display name.
-                </FormDescription> */}
                   <FormMessage />
                 </FormItem>
               )}
             />
 
+            {/* Password Field */}
             <FormField
               control={form.control}
               name="password"
+              rules={{
+                required: "Password is required",
+                minLength: {
+                  value: 6,
+                  message: "Password must be at least 6 characters",
+                },
+              }}
               render={({ field }) => (
                 <FormItem>
                   <FormLabel>Password</FormLabel>
                   <FormControl>
-                    <PasswordInput placeholder="password" {...field} />
+                    <PasswordInput placeholder="Password" {...field} />
                   </FormControl>
-                  {/* <FormDescription>Enter your password.</FormDescription> */}
                   <FormMessage />
                 </FormItem>
               )}
@@ -87,6 +124,7 @@ export default function SignUp() {
               Submit
             </Button>
           </form>
+
           <div className="pt-5 text-center">
             <div className="flex w-full justify-between items-center gap-3">
               <div className="border-2 w-full"></div> <span>X</span>{" "}
