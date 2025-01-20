ac58adc6d36770d5afae8e60827ee0ba07412ddb
mdrakibmia99 <rkrakibhasan680@gmail.com>
2025-01-20 21:16:05 +0600

    create quiz type

---
+++ src/types/quiz.type.ts
@@ -0,0 +1,16 @@
+export interface IQuizResponse {
+  _id: string;
+  title: string;
+  description: string;
+  questions: IQuestion;
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
+}
+
+export interface IQuestion {
+  question: string;
+  options: string[];
+  correctAnswer: string;
+  _id: string;
+}
