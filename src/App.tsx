26ec4a5e11b69e8aeb01b2154b1caa5d70b97a92
mdrakibmia99 <rkrakibhasan680@gmal.com>
2025-02-11 01:00:46 +0600

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,7 @@ import MainLayout from "./components/layout/MainLayout";
 
 
 export default function App() {
-  // const { quizComplete, moreResultInfo } = useAppSelector(
-  //   (state) => state.quiz
-  // );
+
   return (
    
       <MainLayout/>
@@ -13,14 +11,3 @@ export default function App() {
   );
 }
 
- {/* <div>
-        <Header />
-        <h1 className="text-center text-9xl my-12">Quiz App</h1>
-        {moreResultInfo ? (
-          <Result />
-        ) : quizComplete ? (
-          <QuizSummery />
-        ) : (
-          <Question />
-        )}
-      </div> */}
